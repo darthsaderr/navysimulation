d4b1608813a3ac7d18a5aac5af7f4149fc809be8
Sadie <saderrs822@gmail.com>
2015-02-23 23:09:03 -0600

    Create atime.h

---
+++ atime.h
@@ -0,0 +1,36 @@
+/*
+* Sadie Jungers
+* header file for atime.cpp
+*/
+
+#ifndef ATIME_H
+#define ATIME_H
+
+#include <iostream>
+#include <ctime>
+
+class ATime {
+		friend std::ostream& operator<<(std::ostream& os,const ATime at);
+		friend ATime operator+(int, const ATime&);
+	public:
+		ATime();
+		ATime(std::string,std::string);  // mm/dd/yyyy  hh:mm:ss
+		ATime(time_t t);
+		std::string getSVal() const;
+
+		//bool isValid() const;
+		bool operator<(const ATime &) const;
+		bool operator>(const ATime &) const;
+		bool operator==(const ATime &) const;
+		bool operator!=(const ATime &) const;
+		ATime operator+(const int) const;
+		ATime operator++();			// preinc add one second
+		ATime operator++(int);			// postinc add one second
+		ATime& operator+=(int);			// add seconds
+		int operator-(const ATime&) const;	// return seconds
+		int operator-(const int) const;		// subtract seconds
+	private:
+		time_t m_time;
+};
+
+#endif
